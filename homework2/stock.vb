1390ca5623c3207937fcc9ce52c92440cdf75c6b
Rachel <rachel.chloe.gibbs@gmail.com>
2019-08-17 09:05:56 -0500

    Update stock.vb

--- homework2/stock.vb
+++ homework2/stock.vb
@@ -1,17 +1,22 @@
 Sub stock():
+'This works perfectly on the test files but keeps returning an overflow error on sheet 2. Did not have enough time to debug.
+'If you remove the ws looping (and change all ws.Cells and ws.Rows to just Cells and Rows) it works on the individual sheets (but still not sheet 2)
 
+'Loop through each worksheet in the file    
 For Each ws In Worksheets
 
+'Set our variables
 Dim lastrow, startrow, startdate, volume, value_row, lastrow2, value_row2, value_row3 As Long
 Dim startprice, endprice, year_change, percent_change, volumestart, volumeend, most_increase, least_increase As Double
 Dim ticker As String
 Dim headers, headers2, headers3
 
+'set first and last rows and the start date
 lastrow = ws.Cells(Rows.Count, 1).End(xlUp).Row
 startrow = 1
 startdate = ws.Range("B2").Value
 
-' Set Headers
+'set headers
 headers = Array("Ticker", "Yearly Change", "Percent Change", "Total Volume")
 ws.Range("I1:L1") = headers
 headers2 = Array("Ticker", "Value")
@@ -19,14 +24,17 @@ ws.Range("P1:Q1") = headers2
 headers3 = Array("Greatest % increase", "Greatest % decrease", "Greatest total volume")
 ws.Range("O2:O4") = Application.Transpose(headers3)
 
+'loop through entire sheet
 For i = 2 To lastrow
-
+    'if it's the start date, set the ticker value, start price and start volume
     If ws.Cells(i, 2).Value = startdate Then
         startrow = startrow + 1
         ticker = ws.Cells(i, 1).Value
         ws.Cells(startrow, 9).Value = ticker
         startprice = ws.Cells(i, 3).Value
         volumestart = ws.Cells(i, 7).Row
+    'if the ticker doesn't match the ticker below it, set end price, calculate year change, calculate percent change, calculate volume change
+    'set calculated values to cells and highlight accordingly
     ElseIf ws.Cells(i, 1).Value <> ws.Cells(i + 1, 1).Value Then
         endprice = ws.Cells(i, 6).Value
         year_change = endprice - startprice
@@ -45,17 +53,20 @@ For i = 2 To lastrow
     Else
         
     End If
-    
+'keep looping    
 Next i
 
+'set a new last row for our new set of columns
 lastrow2 = ws.Cells(Rows.Count, 11).End(xlUp).Row
 most_increase = ws.Cells(2, 11).Value
 least_increase = ws.Cells(2, 11).Value
 
+'set the max volume change
 ws.Range("Q4").Value = "=MAX(" & ws.Range("L2", "L" & lastrow2).Address(False, False) & ")"
 
+'loop through rows in new columns
 For i = 2 To lastrow2
-    
+    'iterate to find max and min and set ticker values
     If ws.Cells(i, 11).Value > most_increase Then
         most_increase = ws.Cells(i, 11).Value
         ws.Range("P2").Value = ws.Cells(i, 9).Value
@@ -69,9 +80,9 @@ For i = 2 To lastrow2
     If Str(ws.Cells(i, 12).Value) = Str(ws.Range("Q4").Value) Then
         ws.Range("P4").Value = ws.Cells(i, 9).Value
     End If
-    
+'keep looping    
 Next i
-
+'go to next worksheet
 Next ws
 
 End Sub
